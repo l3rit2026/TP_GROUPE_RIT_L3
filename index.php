fc8c5baa4aa11f5a9ed5eb476b5051a70b1d5761
l3rit2026 <isesodl3rit@gmail.com>
2026-03-17 13:08:32 +0100

    baraka mbilizi salomon

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP_EN_GROUPE</title>
+</head>
+<body>
+    <!-- VOS COMMIT C'EST VOS NOM -->
+
+    
+</body>
+</html>
